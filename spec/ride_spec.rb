401be0465594dbccae64c8389735790861b1153a
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-09 11:35:38 -0500

    Test: board_rider test for Ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe Rides do
+RSpec.describe Ride do
    describe '#initialize' do
       it 'exists' do
          ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
@@ -36,6 +36,30 @@ RSpec.describe Rides do
          ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
 
          expect(ride1.total_revenue).to eq(0)
+      end
+
+      it 'starts with an empty rider log' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+         expect(ride1.rider_log).to eq({})
+      end
+   end
+
+   describe '#board_rider' do
+      it 'it boards the ride and adds to the log' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+
+         expect(ride1.board_rider(visitor1)).to eq({visitor1 => 1})
+
+         ride1.board_rider(visitor1)
+         ride1.board_rider(visitor2)
+
+         expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
       end
    end
 end
