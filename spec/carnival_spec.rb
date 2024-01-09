a7107f1259e1a5184b3d5507872bdc3388483589
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-09 12:33:18 -0500

    test: total revenue for carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -86,9 +86,9 @@ RSpec.describe Carnival do
          ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
          ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
          ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
-         visitor1 = Visitor.new('Bruce', 54, '$10')
-         visitor2 = Visitor.new('Tucker', 54, '$5')
-         visitor3 = Visitor.new('Penny', 64, '$15')
+         visitor1 = Visitor.new('Bruce', 54, '$20')
+         visitor2 = Visitor.new('Tucker', 54, '$20')
+         visitor3 = Visitor.new('Penny', 64, '$20')
          carnival.add_ride(ride1)
          carnival.add_ride(ride2)
          carnival.add_ride(ride3)
@@ -111,6 +111,40 @@ RSpec.describe Carnival do
          ride3.board_rider(visitor2)
 
          expect(carnival.most_profitable_ride).to eq(ride2)
+      end
+   end
+
+   describe '#total_revenue' do
+      it 'sums profit from all the rides' do
+         carnival = Carnival.new(30)
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$20')
+         visitor2 = Visitor.new('Tucker', 54, '$20')
+         visitor3 = Visitor.new('Penny', 64, '$20')
+         carnival.add_ride(ride1)
+         carnival.add_ride(ride2)
+         carnival.add_ride(ride3)
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         ride1.board_rider(visitor1)
+         ride1.board_rider(visitor2)
+         ride1.board_rider(visitor1)
+
+         ride2.board_rider(visitor1)
+         ride2.board_rider(visitor1)
+
+         ride3.board_rider(visitor3)
+         ride3.board_rider(visitor3)
+         ride3.board_rider(visitor2)
+         ride3.board_rider(visitor2)
+
+         expect(carnival.total_revenue).to eq(21)
       end
    end
 end
