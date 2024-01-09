2eb5dd4dbe45930d3cc1aac52f16f40baa39a3cb
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-09 12:01:38 -0500

    test: include integration tests to board_rider

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -61,5 +61,92 @@ RSpec.describe Ride do
 
          expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
       end
+
+      it 'will not board visitor that do not have that preference' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+         visitor3 = Visitor.new('Penny', 64, '$15')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         expect(ride3.board_rider(visitor1)).to eq nil
+      end
+
+      it 'will charge visitor' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+         visitor3 = Visitor.new('Penny', 64, '$15')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         ride3.board_rider(visitor3)
+
+         expect(visitor3.spending_money).to eq(13)
+      end
+
+      it 'will add revenue to total revenue' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+         visitor3 = Visitor.new('Penny', 64, '$15')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         ride3.board_rider(visitor3)
+         ride3.board_rider(visitor3)
+
+         expect(ride3.total_revenue).to eq(4)
+      end
+
+      it 'will not board riders smaller than min height' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+         visitor3 = Visitor.new('Penny', 64, '$15')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         
+         expect(ride3.board_rider(visitor2)).to eq nil
+      end
+
+      it 'will not board riders with no money' do
+         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+         ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+         ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+         visitor1 = Visitor.new('Bruce', 54, '$10')
+         visitor2 = Visitor.new('Tucker', 36, '$5')
+         visitor3 = Visitor.new('Penny', 64, '$1')
+
+         visitor1.add_preference(:gentle)
+         visitor2.add_preference(:gentle)
+         visitor2.add_preference(:thrilling)
+         visitor3.add_preference(:thrilling)
+
+         
+         expect(ride3.board_rider(visitor3)).to eq nil
+      end
    end
 end
